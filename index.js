6cf9f44743c224c1ab3dfdaf3877524622e444a7
MakeNowJust <make.just.on@gmail.com>
2014-03-09 11:56:53 +0900

    create main file

---
+++ index.js
@@ -0,0 +1,21 @@
+var
+path = require('path');
+
+var
+SLASH     = '/',
+BACKSLASH = '\\';
+
+var
+sep  = exports.sep = path.sep,
+rSep = exports.rSep = new RegExp('\\' + sep, 'g'),
+seps = exports.seps = 'darwin freebsd linux sunos'.split(' ')
+  .reduce(function (seps, platform) {
+    seps[platform] = SLASH;
+    return seps;
+  }, {win32: BACKSLASH});
+
+exports.normalize = function (p, useSep) {
+  useSep = useSep || SLASH;
+  return path.normalize(p).replace(rSep, useSep);
+};
+
